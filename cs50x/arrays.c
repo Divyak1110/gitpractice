21567f266096e781ba3eb006814077556a750d54
DIVYA K <kotadivya35@gmail.com>
2026-02-26 17:34:37 -0500

    Add files via upload

---
+++ cs50x/arrays.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int const size = 5
+int main(void)
+{
+// Declare an array
+    int numbers[size];
+    numbers[0] = 1;
+// Populate an array
+    for (int i = 1 ; i < size ; i++)
+    {
+        numbers[i] = numbers [i - 1] * 2;
+    }
+    for (int i = 0; i < size; i++)
+    {
+        printf("%i,\n", numbers[i]);
+
+    }
+}
